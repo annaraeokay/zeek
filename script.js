0e4b603edc47a2d76c2e6e3e117fed5116a96311
annaraeokay <annaraelandsman@gmail.com>
2025-03-12 23:15:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,3 @@ function gameLoop() {
 }
 
 gameLoop();
-const zeekImage = new Image();
-zeekImage.src = 'zeek.png';
-zeekImage.onload = () => console.log('Zeek image loaded!');
-zeekImage.onerror = () => console.log('Error loading zeek.png');
